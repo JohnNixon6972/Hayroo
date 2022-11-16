d8dca273fecd4276c90d247fda48bc0e017df2d3
John Nixon <johnbrightnixon@gmail.com>
2022-11-16 09:38:09 +0530

    Added frontend

---
+++ src/components/contact-us/contact_us.js
@@ -0,0 +1,59 @@
+import React from 'react';
+ 
+  function Contact_us() {
+    return (
+       <div>
+      
+ 
+     
+    <section class="bg-light mt-5" id="tourist">    
+     <div class="container">
+      <div class="row text-center">
+        <div class="col-sm-12 col-md-12 mb-4">
+            <h3 class="text-center mt-4 text-secondary">Know the Developers</h3>
+         </div>
+        <div class="col-md-4">
+          <div class="testimonial mb-5">
+           <div class="avatar mx-auto">
+            <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(1).jpg" class="rounded-circle z-depth-1 img-fluid"/>
+          </div>
+          <h4 class="font-weight-bold dark-grey-text mt-4">Anna Deynah</h4>
+          <h6 class="font-weight-bold blue-text my-3">Tourist</h6>
+          <p class="font-weight-normal dark-grey-text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+            eos id officiis hic tenetur quae quaerat ad velit ab hic tenetur.</p>
+        </div>
+      </div>
+ 
+      <div class="col-md-4">
+        <div class="testimonial mb-5">
+          <div class="avatar mx-auto">
+            <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(8).jpg" class="rounded-circle z-depth-1 img-fluid"/>
+          </div>
+          <h4 class="font-weight-bold dark-grey-text mt-4">John Doe</h4>
+          <h6 class="font-weight-bold blue-text my-3">Tourist</h6>
+          <p class="font-weight-normal dark-grey-text">Ut enim ad minima veniam, quis nostrum exercitationem ullam
+            corporis suscipit laboriosam, nisi ut aliquid commodi.</p>
+        </div>
+ 
+      </div>
+      <div class="col-md-4">
+        <div class="testimonial mb-5">
+          <div class="avatar mx-auto">
+            <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(10).jpg" class="rounded-circle z-depth-1 img-fluid"/>
+          </div>
+          <h4 class="font-weight-bold dark-grey-text mt-4">Maria Kate</h4>
+          <h6 class="font-weight-bold blue-text my-3">Toursit</h6>
+          <p class="font-weight-normal dark-grey-text">
+            At vero eos et accusamus et iusto odio dignissimos ducimus qui
+            blanditiis praesentium voluptatum deleniti atque corrupti.</p>
+        </div>
+        </div>
+      </div>
+    </div> 
+   </section> 
+   </div>
+    );
+  }
+   
+  export default Contact_us;
